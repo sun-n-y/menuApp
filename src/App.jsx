220c9388ac489d81d957d61aeefd022763d46955
sunny patel <sunnykpatel.skp@gmail.com>
2023-10-28 11:59:46 -0400

    add menu components and render items

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
+import { useState } from 'react';
 import Title from './Title';
+import menu from './data';
+import Menu from './Menu';
 
 const App = () => {
+  const [menuItems, setMenuItems] = useState(menu);
+
   return (
     <main>
       <section className="menu">
         <Title text={'our menu'} />
+        <Menu items={menuItems} />
       </section>
     </main>
   );
---
+++ src/MenuItem.jsx
@@ -0,0 +1,15 @@
+const MenuItem = ({ title, price, img, desc }) => {
+  return (
+    <article className="menu-item">
+      <img src={img} alt={title} className="img" />
+      <div className="item-info">
+        <header>
+          <h5>{title}</h5>
+          <span className="item-price">${price}</span>
+        </header>
+        <p className="item-text">{desc}</p>
+      </div>
+    </article>
+  );
+};
+export default MenuItem;
