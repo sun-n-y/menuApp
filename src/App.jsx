9d6c4c5aa1f9ebabbb2523a5ae10ab27b26b4adf
sunny patel <sunnykpatel.skp@gmail.com>
2023-10-28 11:34:56 -0400

    add title component with props

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,12 @@
+import Title from './Title';
+
 const App = () => {
-  return <h2>Menu Starter</h2>;
+  return (
+    <main>
+      <section className="menu">
+        <Title text={'our menu'} />
+      </section>
+    </main>
+  );
 };
 export default App;
