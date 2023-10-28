98fa1d171ff5756385cc8d093b6642a209fe523b
sunny patel <sunnykpatel.skp@gmail.com>
2023-10-28 12:49:51 -0400

    add button functionality

--- src/Categories.jsx
+++ src/Categories.jsx
@@ -1,9 +1,14 @@
-const Categories = ({ categories }) => {
+const Categories = ({ categories, filterItems }) => {
   return (
     <div className="btn-container">
       {categories.map((category) => {
         return (
-          <button type="button" className="btn" key={category}>
+          <button
+            type="button"
+            className="btn"
+            key={category}
+            onClick={() => filterItems(category)}
+          >
             {category}
           </button>
         );
