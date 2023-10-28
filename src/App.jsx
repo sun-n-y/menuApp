eb46b2684c4efafd61897999e515f29814243af9
sunny patel <sunnykpatel.skp@gmail.com>
2023-10-28 12:32:50 -0400

    add unique categories logic

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,12 @@ import Title from './Title';
 import menu from './data';
 import Menu from './Menu';
 
+const allCategories = ['all', ...new Set(menu.map((item) => item.category))];
+
 const App = () => {
   const [menuItems, setMenuItems] = useState(menu);
-
+  const [categories, setCategories] = useState(allCategories);
+  console.log(categories);
   return (
     <main>
       <section className="menu">
